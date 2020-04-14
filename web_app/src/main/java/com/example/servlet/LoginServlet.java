83793c0a13e57805aeed4c4dda4188650b5c3491
srt <3268543958@qq.com>
2020-04-14 22:33:20 +0800

    maven webapp

--- web_app/src/main/java/com/example/servlet/LoginServlet.java
+++ web_app/src/main/java/com/example/servlet/LoginServlet.java
@@ -24,6 +24,7 @@ public class LoginServlet extends HttpServlet {
 
         //查询用户名和密码是否在数据库中
 
+        resp.setCharacterEncoding("UTF-8");
         if(userSericeImp.findUserByNameAndPassword(username,password)==null){
             resp.getWriter().println("用户不存在或用户名密码错误");
         }
